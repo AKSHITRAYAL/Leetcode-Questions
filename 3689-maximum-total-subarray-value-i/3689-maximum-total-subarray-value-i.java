b4913a74f59fb89f8baea3f472a80e08d03cbc17
AKSHITRAYAL <137927348+AKSHITRAYAL@users.noreply.github.com>
2026-06-10 00:04:50 +0530

    Time: 1 ms (100%), Space: 62.2 MB (51.08%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -0,0 +1,17 @@
+    class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = nums[0];
+        int min = nums[0];
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+
+        return 1L * k * (max - min);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
